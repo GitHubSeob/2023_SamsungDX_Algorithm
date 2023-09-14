df71a9a1dd6c63dbc18a01034c325ba25a29addd
GitHubSeob <githubseob@gmail.com>
2023-09-15 03:48:52 +0900

    분할정복

---
+++ 10_분할정복/사탕 분배.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <algorithm>
+#define ll long long
+using namespace std;
+ll sum;
+
+ll get_K(int K) {
+	if (K == 1) return 2;
+
+	ll ret(0);	
+	ret = get_K(K / 2);
+	ret = (ret * ret) % sum;
+
+	if (K % 2 == 1) {
+		ret *= 2;
+		ret %= sum;
+	}
+
+	return ret;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T(0);
+	cin >> T;
+	for (int test_case = 1; test_case <= T; ++test_case) {
+		ll A(0), B(0), K(0), answer(0);
+
+		cin >> A >> B >> K;
+
+		if (A > B) swap(A, B);
+		sum = A + B;
+
+		K = get_K(K);
+
+		A = (A * K) % sum;
+		B = sum - A;
+
+		answer = min(A, B);
+
+		cout << "#" << test_case << " " << answer << '\n';
+	}
+
+	return 0;
+}
---
+++ 10_분할정복/염라대왕의 이름 정렬.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+using namespace std;
+
+string name[20000];
+string tmp[20000];
+
+void mergesort(int left, int right) {
+	if (left >= right) return;
+	int mid = (left + right) / 2;
+
+	mergesort(left, mid);
+	mergesort(mid + 1, right);
+
+	int idx = left, m = mid + 1;	
+
+	for (int i = left; i <= right; ++i) {
+		tmp[i] = name[i];
+	}
+
+	while (left <= mid || m <= right) {
+		if (left > mid) {
+			while (m <= right) {
+				name[idx++] = tmp[m++];
+			}
+			break;
+		}
+		else if (m > right) {
+			while (left <= mid) {
+				name[idx++] = tmp[left++];
+			}
+			break;
+		}
+		else {
+			if (tmp[left].size() == tmp[m].size()) {
+				if (tmp[left] < tmp[m]) {
+					name[idx++] = tmp[left++];
+				}
+				else {
+					name[idx++] = tmp[m++];
+				}
+			}
+			else {
+				if (tmp[left].size() < tmp[m].size()) {
+					name[idx++] = tmp[left++];
+				}
+				else {
+					name[idx++] = tmp[m++];
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+
+	int T(0), answer(0);
+	cin >> T;
+	for (int test_case = 1; test_case <= T; ++test_case) {
+		int N(0);
+		cin >> N;
+
+		for (int idx = 0; idx < 20000; ++idx) {
+			name[idx].clear();
+			tmp[idx].clear();
+		}		
+
+		for (int idx = 0; idx < N; ++idx) {
+			cin >> name[idx];
+		}		
+
+		mergesort(0, N - 1);
+
+		cout << "#" << test_case << '\n';
+		cout << name[0] << '\n';
+		for (int idx = 1; idx < N; ++idx) {
+			if (name[idx] == name[idx - 1]) continue;
+			cout << name[idx] << '\n';
+		}
+	}
+}
