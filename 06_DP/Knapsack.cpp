bb841ff8f9d2c7b693e37bb021c3f940e696f687
GitHubSeob <githubseob@gmail.com>
2023-09-15 03:48:00 +0900

    DP

---
+++ 06_DP/Knapsack.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#define pii pair<int, int>
+#define V first
+#define C second
+using namespace std;
+
+int main() {
+	int T(0);
+	cin >> T;
+	for (int test_case = 1; test_case <= T; ++test_case) {
+		int N(0), K(0);
+		cin >> N >> K;
+		vector<pii>item(N, { 0,0 });
+		vector<int>knapsack(K + 1, 0);
+
+		for (int idx = 0; idx < N; ++idx) {
+			cin >> item[idx].V >> item[idx].C;
+		}
+
+		for (int idx = 0; idx < N; ++idx) {
+			for (int weight = K; weight > 0; --weight) {
+				if (weight - item[idx].V < 0) continue;
+				knapsack[weight] = max(knapsack[weight], knapsack[weight - item[idx].V] + item[idx].C);
+			}
+		}
+
+		cout << "#" << test_case << " " << knapsack[K] << '\n';
+	}
+}
---
+++ 06_DP/최장 공통 부분 수열.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int T(0);
+	cin >> T;
+	for (int test_case = 1; test_case <= T; ++test_case) {
+		string s1(""), s2("");
+		cin >> s1 >> s2;
+		vector<vector<int>>DP(s2.size() + 1, vector<int>(s1.size() + 1, 0));
+		for (int idx2 = 1; idx2 <= s2.size(); ++idx2) {
+			for (int idx = 1; idx <= s1.size(); ++idx) {
+				if (s2[idx2 - 1] == s1[idx - 1]) {
+					DP[idx2][idx] = DP[idx2 - 1][idx - 1] + 1;
+				}
+				else {
+					DP[idx2][idx] = max(DP[idx2][idx - 1], DP[idx2 - 1][idx]);
+				}
+			}
+		}
+		int answer = DP[s2.size()][s1.size()];
+		cout << "#" << test_case << " " << answer << '\n';
+	}
+}
